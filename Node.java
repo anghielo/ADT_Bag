a494a7689f0dd6ad6901234d89a34c9d601e6484
anghielo <anghielo@yahoo.com>
2021-02-16 02:51:24 -0800

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,35 @@
+// Dany Flores
+// CS2400, Section 01
+// Project 1 - ADT Bag
+// 05 October 2020
+
+public class Node<T> {		// Non-inner class, therefore we must specify generic data type <T> 
+	private T data;			// Entry in bag
+	private Node<T> next;	// Link to the next node
+	
+	// The constructor's name is Node, not Node<T>
+	public Node(T dataPortion) {
+		this(dataPortion, null);
+	}
+	
+	public Node(T dataPortion, Node<T> nextNode) {
+		data = dataPortion;
+		next = nextNode;
+	}
+	
+	public T getData() {
+		return data;
+	}
+	
+	public void setData(T newData) {
+		data = newData;
+	}
+	
+	public Node<T> getNextNode() {
+		return next;
+	}
+	
+	public void setNextNode(Node<T> nextNode) {
+		next = nextNode;
+	}
+}
